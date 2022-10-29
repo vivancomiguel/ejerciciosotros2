978f4961fba818a7bd63311560b31d049e24f558
vivancomiguel <113225991+vivancomiguel@users.noreply.github.com>
2022-10-28 20:15:07 -0500

    fix: Funcion principal de la aplicacion

--- formulario.js
+++ formulario.js
@@ -1,9 +1,9 @@
-/* No le emplie mucho tiempo pero hice algunas modificaciones */
-const formulario = document.querySelector("#form");
+/* La funcion consiste en agregar invitados siempre y cuando el formulario se realice correctamente */
+const formulario = document.querySelector(".formulario"); // Se cambio #form por .formulario
 
 formulario.onsubmit = function(e) {
-
-  e.prevent();
+  e.preventDefault();
+  console.log("envia")
   
   // Los elementos se son declarados con const o let em lugar de const, se renombran y se incluye ;
   // Se identan los elementos
@@ -11,11 +11,10 @@ formulario.onsubmit = function(e) {
   const elemEdad = formulario.elements[1];
   const elemNacionalidad = formulario.elements[2];
 
-  let nombre = elemNombre.value;
+  const nombre = elemNombre.value; // Cambia let por const
   const edad = elemEdad.value;
-
-  let i = elemNacionalidad.selectedIndex;
-  let nacionalidad = elemNacionalidad.options[i].value;
+  const selectedNacionalidad = elemNacionalidad.selectedIndex; // Cambie i por un nombre mas descriptivo
+  const nacionalidad = elemNacionalidad.options[selectedNacionalidad].value;
   console.log(nombre, edad);
   console.log(nacionalidad);
 
@@ -30,14 +29,15 @@ formulario.onsubmit = function(e) {
     (edad > 18 && edad < 120) ) {
     agregarInvitado(nombre, edad, nacionalidad);
   }
-  // Este bloque se metio dentro de la funcion de submit
-  const botonBorrar = document.createElement("button");
-  botonBorrar.textContent = "Eliminar invitado";
-  botonBorrar.id = "boton-borrar";
-  const corteLinea = document.createElement("br");
-  document.body.appendChild(corteLinea);
-  document.body.appendChild(botonBorrar);
 }
+
+// Este bloque esta extra
+/*const botonBorrar = document.createElement("button");
+botonBorrar.textContent = "Eliminar invitado";
+botonBorrar.id = "boton-borrar";
+const corteLinea = document.createElement("br");
+document.body.appendChild(corteLinea);
+document.body.appendChild(botonBorrar);*/
 
 
 function agregarInvitado(nombre, edad, nacionalidad) {
@@ -54,11 +54,12 @@ function agregarInvitado(nombre, edad, nacionalidad) {
   else if (nacionalidad === "per") {
     nacionalidad = "Peruana";
   }
-
 const lista = document.getElementById("lista-de-invitados");
 
 const elementoLista = document.createElement("div");
-elementoLista.classList.added("elemento-lista");
+elementoLista.classList.add("elemento-lista"); // En vez de added es add
+console.log(elementoLista);
+console.log(lista);
 lista.appendChild(elementoLista);
 
 const spanNombre = document.createElement("span");
@@ -71,16 +72,15 @@ elementoLista.appendChild(inputNombre);
 elementoLista.appendChild(espacio);
 
 function crearElemento(descripcion, valor) {
-const spanNombre = document.createElement("span");
-const inputNombre = document.createElement("input");
-const espacio = document.createElement("br");
-spanNombre.textContent = descripcion + ": ";
-inputNombre.value = valor ;
-elementoLista.appendChild(spanNombre);
-elementoLista.appendChild(inputNombre);
-elementoLista.appendChild(espacio);
+  const spanNombre = document.createElement("span");
+  const inputNombre = document.createElement("input");
+  const espacio = document.createElement("br");
+  spanNombre.textContent = descripcion + ": ";
+  inputNombre.value = valor ;
+  elementoLista.appendChild(spanNombre);
+  elementoLista.appendChild(inputNombre);
+  elementoLista.appendChild(espacio);
 }
-
 crearElemento("Nombre", nombre);
 crearElemento("Edad", edad);
 crearElemento("Nacionalidad", nacionalidad);
